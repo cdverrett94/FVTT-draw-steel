86a49d4448c0c24abf0baf4b8dadeec39e5ba4eb
Kody Verrett <cdverrett94@gmail.com>
2023-12-30 16:34:59 -0600

    put more data fields into base actor instead of hero actor

--- module/documents/actors/base/data-model.js
+++ module/documents/actors/base/data-model.js
@@ -1,3 +1,5 @@
+import { characteristics } from '../../../constants.js';
+
 export class MCDMActorData extends foundry.abstract.DataModel {
     static defineSchema() {
         const fields = foundry.data.fields;
@@ -101,6 +103,130 @@ export class MCDMActorData extends foundry.abstract.DataModel {
                 required: true,
                 initial: 1,
                 integer: true,
+            }),
+            speed: new fields.NumberField(),
+            reach: new fields.NumberField(),
+            skills: new fields.SchemaField({
+                acrobatics: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'agility',
+                        choices: characteristics,
+                    }),
+                }),
+                athletics: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'might',
+                        choices: characteristics,
+                    }),
+                }),
+                charm: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'presence',
+                        choices: characteristics,
+                    }),
+                }),
+                craft: new fields.ArrayField(
+                    new fields.SchemaField({
+                        subskill: new fields.StringField({
+                            required: true,
+                            nullable: false,
+                        }),
+                        proficient: new fields.BooleanField({
+                            initial: false,
+                        }),
+                        characteristic: new fields.StringField({
+                            initial: 'reason',
+                            choices: characteristics,
+                        }),
+                    })
+                ),
+                deceive: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'presence',
+                        choices: characteristics,
+                    }),
+                }),
+                empathy: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'intuition',
+                        choices: characteristics,
+                    }),
+                }),
+                intimidate: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'presence',
+                        choices: characteristics,
+                    }),
+                }),
+                knowledge: new fields.ArrayField(
+                    new fields.SchemaField({
+                        subskill: new fields.StringField({
+                            required: true,
+                            nullable: false,
+                        }),
+                        proficient: new fields.BooleanField({
+                            initial: false,
+                        }),
+                        characteristic: new fields.StringField({
+                            initial: 'reason',
+                            choices: characteristics,
+                        }),
+                    })
+                ),
+                notice: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'intuition',
+                        choices: characteristics,
+                    }),
+                }),
+                skulduggery: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'agility',
+                        choices: characteristics,
+                    }),
+                }),
+                stealth: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'agility',
+                        choices: characteristics,
+                    }),
+                }),
+                vigor: new fields.SchemaField({
+                    proficient: new fields.BooleanField({
+                        initial: false,
+                    }),
+                    characteristic: new fields.StringField({
+                        initial: 'might',
+                        choices: characteristics,
+                    }),
+                }),
             }),
         };
     }
--- module/documents/actors/heroes/data-model.js
+++ module/documents/actors/heroes/data-model.js
@@ -1,4 +1,3 @@
-import { characteristics } from '../../../constants.js';
 import { MCDMActorData } from '../base/data-model.js';
 
 export class HeroData extends MCDMActorData {
@@ -36,144 +35,12 @@ export class HeroData extends MCDMActorData {
                     nullable: false,
                 }),
             }),
-            resources: new fields.SchemaField({
-                one: new fields.NumberField({
-                    required: true,
-                    initial: 0,
-                    min: 0,
-                    integer: true,
-                    nullable: false,
-                }),
-                two: new fields.NumberField({
-                    required: true,
-                    initial: 0,
-                    min: 0,
-                    integer: true,
-                    nullable: false,
-                }),
-            }),
-            skills: new fields.SchemaField({
-                acrobatics: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'agility',
-                        choices: characteristics,
-                    }),
-                }),
-                athletics: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'might',
-                        choices: characteristics,
-                    }),
-                }),
-                charm: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'presence',
-                        choices: characteristics,
-                    }),
-                }),
-                craft: new fields.ArrayField(
-                    new fields.SchemaField({
-                        subskill: new fields.StringField({
-                            required: true,
-                            nullable: false,
-                        }),
-                        proficient: new fields.BooleanField({
-                            initial: false,
-                        }),
-                        characteristic: new fields.StringField({
-                            initial: 'reason',
-                            choices: characteristics,
-                        }),
-                    })
-                ),
-                deceive: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'presence',
-                        choices: characteristics,
-                    }),
-                }),
-                empathy: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'intuition',
-                        choices: characteristics,
-                    }),
-                }),
-                intimidate: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'presence',
-                        choices: characteristics,
-                    }),
-                }),
-                knowledge: new fields.ArrayField(
-                    new fields.SchemaField({
-                        subskill: new fields.StringField({
-                            required: true,
-                            nullable: false,
-                        }),
-                        proficient: new fields.BooleanField({
-                            initial: false,
-                        }),
-                        characteristic: new fields.StringField({
-                            initial: 'reason',
-                            choices: characteristics,
-                        }),
-                    })
-                ),
-                notice: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'intuition',
-                        choices: characteristics,
-                    }),
-                }),
-                skulduggery: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'agility',
-                        choices: characteristics,
-                    }),
-                }),
-                stealth: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'agility',
-                        choices: characteristics,
-                    }),
-                }),
-                vigor: new fields.SchemaField({
-                    proficient: new fields.BooleanField({
-                        initial: false,
-                    }),
-                    characteristic: new fields.StringField({
-                        initial: 'might',
-                        choices: characteristics,
-                    }),
-                }),
-            }),
+            currentResources: new fields.ArrayField(
+                new fields.SchemaField({
+                    name: new fields.StringField(),
+                    current: new fields.NumberField(),
+                })
+            ),
         };
     }
 }
