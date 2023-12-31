e799b3bc6874192564d498e4f2ff48c0a0c8f4d1
Kody Verrett <cdverrett94@gmail.com>
2023-12-31 00:01:58 -0600

    add more ability types

--- module/constants.js
+++ module/constants.js
@@ -2,6 +2,10 @@ const characteristics = ['might', 'agility', 'endurance', 'reason', 'intuition',
 const skills = ['acrobatics', 'athletics', 'charm', 'craft', 'deceive', 'empathy', 'intimidate', 'knowledge', 'notice', 'skulduggery', 'stealth', 'vigor'];
 const keywords = ['attack', 'kit', 'magic', 'psionic', 'weapon'];
 const damageTypes = ['untyped', 'acid', 'cold', 'corruption', 'fire', 'holy', 'lightning', 'poison', 'psychic', 'sonic'];
-const abilityTypes = ['basic', 'signature', 'heroic', 'triggered'];
+const baseAbilityTypes = ['action', 'basic', 'manuever', 'triggered'];
+const heroAbilityTypes = ['signature', 'heroic'];
+const monsterAbilityTypes = ['villain'];
+
+const abilityTypes = [...baseAbilityTypes, ...heroAbilityTypes, ...monsterAbilityTypes].sort();
 
 export { abilityTypes, characteristics, damageTypes, keywords, skills };
