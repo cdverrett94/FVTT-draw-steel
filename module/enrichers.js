9432b45e9128e94785fd5085937d69232dea6997
Kody Verrett <cdverrett94@gmail.com>
2024-01-03 23:56:35 -0600

    start building out test and resistance enrichers

--- module/enrichers.js
+++ module/enrichers.js
@@ -8,8 +8,35 @@ export function registerCustomEnrichers() {
         enricher: enrichDamage,
     });
 
-    document.body.addEventListener('click', rollDamage);
+    CONFIG.TextEditor.enrichers.push({
+        pattern: /@Test\[(?<formula>[^\]\|]+)(?:\|*(?<config>.*))\]/gi,
+        enricher: enrichTest,
+    });
+
+    CONFIG.TextEditor.enrichers.push({
+        pattern: /@Resistance\[(?<formula>[^\]\|]+)(?:\|*(?<config>.*))\]/gi,
+        enricher: enrichResistance,
+    });
+
+    document.body.addEventListener('click', rollAction);
     // @Damage[1d6+2|type=holy|traits=Attack,Magic]
+}
+
+function _getEnrichedOptions(match, options) {
+    let { formula, config } = match.groups;
+    let data = {};
+    config = config.split('|');
+    config.forEach((option) => {
+        let [key, value] = option.split('=');
+        data[key] = value;
+    });
+
+    data.formula = formula;
+    data.actor = options.actor;
+    data.actorId = data.actor?.uuid;
+    data.replaceCharacteristic = data.replaceCharacteristic === 'false' ? false : true;
+
+    return data;
 }
 
 function enrichDamage(match, options) {
@@ -22,32 +49,63 @@ function enrichDamage(match, options) {
     banes = number of banes to apply to roll
     impacts = number of impact dice to roll
     */
-    let { formula, config } = match.groups;
-    let data = {};
-    config = config.split('|');
-    config.forEach((option) => {
-        let [key, value] = option.split('=');
-        data[key] = value;
-    });
+    let data = _getEnrichedOptions(match, options);
 
     data.type = damageTypes.includes(data.type) ? data.type : 'untyped';
-    data.formula = formula;
-    data.actor = options.actor;
-    data.actorId = data.actor?.uuid;
-    data.replaceCharacteristic = data.replaceCharacteristic === 'false' ? false : true;
     data.applyKitDamage = data.applyKitDamage === 'false' ? false : true;
+    if (data.replaceCharacteristic) data.formula = new DamageRoll(DamageRoll.constructFinalFormula(data.formula, data), {}, data)._formula;
 
-    return createRollLink(formula, data);
+    let link = createRollLink('damage', data.formula, data);
+
+    link.innerHTML = `<i class="fa-solid fa-dice-d6"></i> ${data.formula}${data.type !== 'untyped' ? ' ' + data.type : ''}`;
+
+    return link;
 }
 
-function createRollLink(formula, dataset) {
+function enrichTest(match, options) {
+    let data = _getEnrichedOptions(match, options);
+
+    data.tn = Number(data.tn) ?? Number(options.tn);
+    if (Number.isNaN(data.tn)) {
+        if (data.tn === 'easy') data.tn = 7;
+        else if (data.tn === 'hard') data.tn = 12;
+        else if (data.tn === 'moderate') data.tn = 9;
+        else data.tn = null;
+    }
+
+    let link = createRollLink('test', data.formula, data);
+    let localizedCharacteristic = game.i18n.localize(`characteristics.${data.characteristic}.label`);
+    let localizedSkill = game.i18n.localize(`skills.${data.skill}.label`);
+    let subskillText = data.subskill ? ` (${data.subskill})` : '';
+    link.innerHTML = `<i class="fa-solid fa-dice-d6"></i> ${data.tn ? data.tn : ''} ${localizedCharacteristic}-${localizedSkill}${subskillText} Test`;
+
+    return link;
+}
+
+function enrichResistance(match, options) {
+    let data = _getEnrichedOptions(match, options);
+
+    data.tn = Number(data.tn) ?? Number(options.tn);
+    if (Number.isNaN(data.tn)) {
+        if (data.tn === 'easy') data.tn = 7;
+        else if (data.tn === 'hard') data.tn = 12;
+        else data.tn = 9;
+    }
+    console.log('resistance data', data);
+
+    let link = createRollLink('resistance', data.formula, data);
+
+    link.innerHTML = `<i class="fa-solid fa-dice-d6"></i> ${data.tn} ${game.i18n.localize('characteristics.' + data.characteristic + '.abbreviation')} Resists`;
+
+    return link;
+}
+
+function createRollLink(type, formula, dataset) {
     const link = document.createElement('a');
     link.classList.add('roll-link');
-    link.classList.add('roll-damage');
-    if (dataset.replaceCharacteristic) formula = new DamageRoll(DamageRoll.constructFinalFormula(formula, dataset), {}, dataset)._formula;
+    link.classList.add(`roll-${type}`);
 
     _addDataset(link, dataset);
-    link.innerHTML = `<i class="fa-solid fa-dice-d6"></i> ${formula}${dataset.type !== 'untyped' ? ' ' + dataset.type : ''}`;
 
     return link;
 }
@@ -56,6 +114,17 @@ function _addDataset(element, dataset) {
     for (const [key, value] of Object.entries(dataset)) {
         if (!['input', 'values', 'actor'].includes(key) && value) element.dataset[key] = value;
     }
+}
+
+async function rollAction(event) {
+    const target = event.target.closest('.roll-link');
+    if (!target) return;
+    const isDamage = target.classList.contains('roll-damage');
+    const isTest = target.classList.contains('roll-test');
+    const isResistance = target.classList.contains('roll-resistance');
+    if (isDamage) rollDamage(event);
+    else if (isTest) rollTest(event);
+    else if (isResistance) rollResistance(event);
 }
 
 async function rollDamage(event) {
@@ -80,4 +149,18 @@ async function rollDamage(event) {
         applyKitDamage,
     };
     await new MCDMRollDialog({ context }).render(true);
+}
+
+async function rollTest(event) {
+    const target = event.target.closest('.roll-link.roll-test');
+    if (!target) return;
+
+    console.log('test');
+}
+
+async function rollResistance(event) {
+    const target = event.target.closest('.roll-link.roll-resistance');
+    if (!target) return;
+
+    console.log('resistance');
 }
