e34acd61899a5927b2d4bd23b7d8b12795433731
Kody Verrett <cdverrett94@gmail.com>
2024-01-09 17:35:34 -0600

    add damage types and action times to constants

--- module/constants.js
+++ module/constants.js
@@ -10,6 +10,18 @@ const characteristicSelectOptions = {
 const skills = ['acrobatics', 'athletics', 'charm', 'craft', 'deceive', 'empathy', 'intimidate', 'knowledge', 'notice', 'skulduggery', 'stealth', 'vigor'];
 const keywords = ['attack', 'kit', 'magic', 'psionic', 'weapon'];
 const damageTypes = ['untyped', 'acid', 'cold', 'corruption', 'fire', 'holy', 'lightning', 'poison', 'psychic', 'sonic'];
+const damageTypeSelectOptions = {
+    untyped: 'damageTypes.untyped.label',
+    acid: 'damageTypes.acid.label',
+    cold: 'damageTypes.cold.label',
+    corruption: 'damageTypes.corruption.label',
+    fire: 'damageTypes.fire.label',
+    holy: 'damageTypes.holy.label',
+    lightning: 'damageTypes.lightning.label',
+    poison: 'damageTypes.poison.label',
+    psychic: 'damageTypes.psychic.label',
+    sonic: 'damageTypes.sonic.label',
+};
 const baseAbilityTypes = ['action', 'basic', 'manuever', 'triggered'];
 const heroAbilityTypes = ['signature', 'heroic'];
 const monsterAbilityTypes = ['passive', 'villain'];
@@ -17,4 +29,24 @@ const monsterRoles = ['ambusher', 'artillery', 'brute', 'bystander', 'controller
 
 const abilityTypes = [...baseAbilityTypes, ...heroAbilityTypes, ...monsterAbilityTypes].sort();
 
-export { abilityTypes, characteristicSelectOptions, characteristics, damageTypes, keywords, monsterRoles, skills };
+const actionTimes = ['action', 'manuever', 'free-manuever', 'triggered', 'free-triggered'];
+const actionTimeSelectOptions = {
+    'action': 'actionTimes.action.label',
+    'manuever': 'actionTimes.manuever.label',
+    'free-manuever': 'actionTimes.free-manuever.label',
+    'triggered': 'actionTimes.triggered.label',
+    'free-triggered': 'actionTimes.free-triggered.label',
+};
+
+export {
+    abilityTypes,
+    actionTimeSelectOptions,
+    actionTimes,
+    characteristicSelectOptions,
+    characteristics,
+    damageTypeSelectOptions,
+    damageTypes,
+    keywords,
+    monsterRoles,
+    skills,
+};
