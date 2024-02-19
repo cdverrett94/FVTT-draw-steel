91ed672eab44006ad764814934289eeeee9add82
Kody Verrett <cdverrett94@gmail.com>
2024-02-19 14:25:33 -0600

    add status effects

--- system.js
+++ system.js
@@ -7,6 +7,7 @@ import { AbilityData } from './module/documents/items/ability/data-model.js';
 import { AbilitySheet } from './module/documents/items/ability/sheet/sheet.js';
 import { AncestryData } from './module/documents/items/ancestry/data-model.js';
 import { AncestrySheet } from './module/documents/items/ancestry/sheet/sheet.js';
+import { ClassData } from './module/documents/items/class/data-model.js';
 import { ClassSheet } from './module/documents/items/class/sheet/sheet.js';
 import { MCDMItemProxy } from './module/documents/items/item-proxy.js';
 import { KitData } from './module/documents/items/kit/data-model.js';
@@ -17,15 +18,18 @@ import { TestRoll } from './module/documents/rolls/test/test-roll.js';
 import { registerCustomEnrichers } from './module/enrichers.js';
 
 Hooks.on('init', () => {
-    // Set module Data Models
+    console.log('hi');
+    // Set System Actor Data Models
     CONFIG.Actor.dataModels.hero = HeroData;
     CONFIG.Actor.dataModels.monster = MonsterData;
 
+    // Set System Item Data Models
     CONFIG.Item.dataModels.ability = AbilityData;
     CONFIG.Item.dataModels.kit = KitData;
     CONFIG.Item.dataModels.ancestry = AncestryData;
+    CONFIG.Item.dataModels.class = ClassData;
 
-    // Set module Document Classes
+    // Set System Document Classes
     CONFIG.Actor.documentClass = MCDMActorProxy;
     CONFIG.Item.documentClass = MCDMItemProxy;
 
@@ -42,6 +46,7 @@ Hooks.on('init', () => {
     ];
     loadTemplates(templatePaths);
 
+    // Register Actor Sheets
     Actors.unregisterSheet('core', ActorSheet);
     Actors.registerSheet('mcdmrpg', HeroSheet, {
         types: ['hero'],
@@ -54,6 +59,7 @@ Hooks.on('init', () => {
         label: 'Monster Sheet',
     });
 
+    // Register Item Sheets
     Items.unregisterSheet('core', ItemSheet);
     Items.registerSheet('mcdmrpg', ClassSheet, {
         types: ['class'],
@@ -75,13 +81,92 @@ Hooks.on('init', () => {
         makeDefault: true,
         label: 'Ancestry Sheet',
     });
+
+    // Add Custom Handlebars helper
     Handlebars.registerHelper('includes', function (array, value) {
         return array.includes(value);
     });
 
+    // Register Enrichers
     registerCustomEnrichers();
 
+    // Register System Dice Rolls
     CONFIG.Dice.rolls.push(DamageRoll, ResistanceRoll, TestRoll);
+
+    // Register System Conditions
+    CONFIG.statusEffects = [
+        {
+            icon: 'icons/magic/death/grave-tombstone-glow-tan.webp',
+            id: 'dead',
+            name: 'conditions.dead',
+        },
+        {
+            icon: 'icons/skills/wounds/blood-drip-droplet-red.webp',
+            id: 'bloodied',
+            name: 'conditions.bloodied',
+        },
+        {
+            icon: 'icons/magic/unholy/strike-body-life-soul-green.webp',
+            id: 'dazed',
+            name: 'conditions.dazed',
+        },
+        {
+            icon: 'icons/magic/unholy/silhouette-robe-evil-power.webp',
+            id: 'frightened',
+            name: 'conditions.frightened',
+        },
+
+        {
+            icon: 'icons/skills/melee/unarmed-punch-fist-yellow-red.webp',
+            id: 'grappled',
+            name: 'conditions.grappled',
+        },
+        {
+            icon: 'icons/magic/life/ankh-gold-blue.webp',
+            id: 'ongoing-damage',
+            name: 'conditions.ongoing-damage',
+        },
+        {
+            icon: 'icons/magic/control/silhouette-fall-slip-prone.webp',
+            id: 'prone',
+            name: 'conditions.prone',
+        },
+        {
+            icon: 'icons/magic/control/debuff-chains-shackle-movement-red.webp',
+            id: 'restrained',
+            name: 'conditions.restrained',
+        },
+        {
+            icon: 'icons/skills/movement/feet-winged-boots-brown.webp',
+            id: 'slowed',
+            name: 'conditions.slowed',
+        },
+        {
+            icon: 'icons/magic/control/fear-fright-white.webp',
+            id: 'surprised',
+            name: 'conditions.surprised',
+        },
+        {
+            icon: 'icons/magic/control/energy-stream-link-spiral-white.webp',
+            id: 'taunted',
+            name: 'conditions.taunted',
+        },
+        {
+            icon: 'icons/magic/control/debuff-energy-snare-blue.webp',
+            id: 'unbalanced',
+            name: 'conditions.unbalanced',
+        },
+        {
+            icon: 'icons/magic/control/sleep-bubble-purple.webp',
+            id: 'unconscious',
+            name: 'conditions.unconscious',
+        },
+        {
+            icon: 'icons/magic/death/hand-withered-gray.webp',
+            id: 'weakened',
+            name: 'conditions.weakened',
+        },
+    ];
 });
 
 Hooks.on('ready', async () => {
