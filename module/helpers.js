448c5e1959d6a8fe19e083396785d59c9fa7bdb4
Kody Verrett <cdverrett94@gmail.com>
2024-02-19 17:13:17 -0600

    add migrate data function for remembering how to migrate invalid docs

--- module/helpers.js
+++ module/helpers.js
@@ -7,4 +7,31 @@ async function getRollActor() {
     return actor;
 }
 
+async function migrateItems(items) {
+    for (const id of items.invalidDocumentIds) {
+        const item = items.getInvalid(id);
+        if (item.type === 'ability') {
+            let updateData;
+            let itemTime = item.validationFailures.fields.fields.system.fields.time.invalidValue;
+            if (itemTime === 'Free Manuever') updateData = 'free-maneuver';
+            else if (itemTime === 'Maneuver') updateData = 'maneuver';
+            else if (itemTime === 'Free Triggered Action') updateData = 'free-triggered';
+            else if (itemTime === 'Free Manuever') updateData = 'free-maneuver';
+            else if (itemTime === 'Maneuver') updateData = 'maneuver';
+            else if (itemTime === 'Free Triggered Action') updateData = 'free-triggered';
+            else if (itemTime === 'Triggered') updateData = 'triggered';
+
+            await item.update({ system: { time: updateData } });
+        }
+    }
+}
+
+async function migrateData() {
+    game.actors.contents.forEach(async (actor) => {
+        migrateItems(actor.items);
+    });
+
+    migrateItems(game.items);
+}
+
 export { getRollActor };
