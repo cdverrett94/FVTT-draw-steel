279a0307c66cfbecc81da3026976aae51a414346
Kody Verrett <cdverrett94@gmail.com>
2024-02-21 19:07:09 -0600

    use appv2 for base roll dialog

--- module/documents/rolls/base/roll-dialog/roll-dialog.js
+++ module/documents/rolls/base/roll-dialog/roll-dialog.js
@@ -1,3 +1,5 @@
+import { characteristics } from '../../../../constants.js';
+
 export class BaseRollDialog extends Application {
     constructor(options) {
         super(options);
@@ -69,5 +71,93 @@ export class BaseRollDialog extends Application {
                 this.close();
             });
         });
+    }
+}
+
+const { HandlebarsApplicationMixin, ApplicationV2 } = foundry.applications.api;
+export class BaseRollDialogV2 extends HandlebarsApplicationMixin(ApplicationV2) {
+    constructor(options = {}) {
+        super(options);
+
+        this.context = {};
+        Object.assign(this.context, options);
+        this.context.boons ??= 0;
+        this.context.banes ??= 0;
+        this.context.impacts ??= 0;
+        this.context.baseFormula ??= 0;
+        this.context.replaceCharacteristic ??= true;
+    }
+
+    get formula() {
+        return this.sheetRoller.constructFinalFormula(this.context.baseFormula, this.context);
+    }
+
+    static additionalOptions = {
+        window: {
+            icon: 'fa-solid fa-dice-d6',
+        },
+        classes: ['roll-dialog'],
+        position: {
+            width: 315,
+        },
+        actions: {
+            adjustDice: BaseRollDialogV2.adjustDice,
+            roll: BaseRollDialogV2.roll,
+        },
+    };
+
+    /** @inheritDoc */
+    static DEFAULT_OPTIONS = foundry.utils.mergeObject(super.DEFAULT_OPTIONS, BaseRollDialogV2.additionalOptions, { inplace: false });
+
+    /** @override */
+    static PARTS = {
+        header: {
+            id: 'header',
+            template: 'systems/mcdmrpg/module/documents/rolls/base/roll-dialog/sheet/header.hbs',
+        },
+        characteristic: {
+            id: 'characteristic-select',
+            template: 'systems/mcdmrpg/module/documents/rolls/base/roll-dialog/sheet/characteristic-select.hbs',
+        },
+        adjustments: {
+            id: 'dice-adjustments',
+            template: 'systems/mcdmrpg/module/documents/rolls/base/roll-dialog/sheet/dice-adjustments.hbs',
+        },
+        roll: {
+            id: 'roll',
+            template: 'systems/mcdmrpg/module/documents/rolls/base/roll-dialog/sheet/roll-button.hbs',
+        },
+    };
+
+    async _prepareContext(options) {
+        return {
+            characteristics,
+            ...this.context,
+            formula: this.formula,
+        };
+    }
+
+    _attachPartListeners(partId, htmlElement, options) {
+        super._attachPartListeners(partId, htmlElement, options);
+        if (partId === 'characteristic') {
+            htmlElement.querySelector('select').addEventListener('change', (event) => {
+                this.context.characteristic = event.target.value;
+                this.render({ parts: ['header', 'roll'] });
+            });
+        }
+    }
+
+    static adjustDice(event, target) {
+        let { type, adjustment } = { ...target.dataset };
+        if (adjustment === 'increase') this.context[type] = Math.max(this.context[type] + 1, 0);
+        else this.context[type] = Math.max(this.context[type] - 1, 0);
+
+        this.render({ parts: ['adjustments', 'roll'] });
+    }
+
+    static async roll() {
+        let roll = await new this.sheetRoller(this.formula, this.actor, this.context).evaluate();
+        roll.toMessage();
+        this.close();
     }
 }
