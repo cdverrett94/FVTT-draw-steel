3fc76161e00a7335bed1122cd91069d7552933a8
Kody Verrett <cdverrett94@gmail.com>
2024-02-21 19:10:45 -0600

    removing unused hp scrolling text

--- module/documents/actors/base/base.js
+++ module/documents/actors/base/base.js
@@ -25,27 +25,26 @@ export class MCDMActor extends Actor {
         this.system.chanceHit = '@Damage[1d4|characteristic=highest]';
 
         this.system.grappleTN = 7 + this.system.might;
-    }
 
-    prepareDerivedData() {
-        super.prepareDerivedData();
+        this.system.conditions = this.items.filter((item) => item.type === 'condition');
 
-        this.system.hp.healing = Math.floor(this.system.hp.max / 3);
-        this.system.hp.bloodied = Math.floor(this.system.hp.max / 2);
-
-        if (this.system.hp.current <= this.system.hp.bloodied) {
+        if (this.system.hp.current <= this.system.hp.bloodied && !this.items.find((condition) => condition.name === 'Bloodied')) {
             // TODO push bloodied condition
         }
-
-        if (this.system.hp.current <= 0 && this.system.hp.current > this.system.hp.bloodied) {
+        if (this.system.hp.current <= 0 && this.system.hp.current > -this.system.hp.bloodied) {
             // TODO push unbalanced condition
         }
 
-        if (this.system.hp.current === this.system.hp.bloodied) {
+        if (this.system.hp.current === -this.system.hp.bloodied) {
             // TODO mark dead
         }
     }
 
+    prepareDerivedData() {
+        super.prepareDerivedData();
+    }
+
+    // Add new craft/knowledge subskill
     async addSkill({ skill, subskill = 'New Skill', characteristic = 'reason', proficient = true } = {}) {
         if (skill !== 'craft' && skill !== 'knowledge') return ui.notifications.error('Skill must be "craft" or "knowledge".');
         if (!(characteristic in characteristics)) return ui.notifications.error('The used characteristic must be a valid one.');
@@ -59,6 +58,7 @@ export class MCDMActor extends Actor {
         return await this.update({ [`system.skills.${skill}`]: skillArray });
     }
 
+    // Delete new craft/knowledge subskill
     async deleteSkill({ skill, subskill } = {}) {
         if (!skill || !subskill) return ui.notifications.error('Must provide a skill and subskill');
         if (skill !== 'craft' && skill !== 'knowledge') return ui.notifications.error('Skill must be "craft" or "knowledge".');
@@ -68,41 +68,5 @@ export class MCDMActor extends Actor {
 
         skillArray.splice(skillIndex, 1);
         return await this.update({ [`system.skills.${skill}`]: skillArray });
-    }
-
-    async _preUpdate(changed, options, user) {
-        if ('hp' in (changed.system || {})) {
-            const currentHp = this.system.hp.current;
-            const changedHp = changed.system.hp.current;
-            const newHp = Math.clamp(changedHp, -this.system.hp.bloodied, this.system.hp.max);
-            const hpDelta = newHp - currentHp;
-            changed.system.hp.current = newHp;
-            options.hpDelta = hpDelta;
-        }
-
-        await super._preUpdate(changed, options, user);
-    }
-
-    _onUpdate(data, options, userId) {
-        super._onUpdate(data, options, userId);
-        this._displayScrollingDamage(options.hpDelta);
-    }
-
-    _displayScrollingDamage(hpDelta) {
-        if (!hpDelta) return;
-        hpDelta = Number(hpDelta);
-        const tokens = this.isToken ? [this.token?.object] : this.getActiveTokens(true);
-        for (const t of tokens) {
-            if (!t.visible || !t.renderable) continue;
-            const pct = Math.clamped(Math.abs(hpDelta) / this.system.hp.max, 0, 1);
-            canvas.interface.createScrollingText(t.center, hpDelta.signedString(), {
-                anchor: CONST.TEXT_ANCHOR_POINTS.TOP,
-                fontSize: 16 + 32 * pct, // Range between [16, 48]
-                fill: hpDelta < 0 ? 'red' : 'green',
-                stroke: 0x000000,
-                strokeThickness: 4,
-                jitter: 0.25,
-            });
-        }
     }
 }
