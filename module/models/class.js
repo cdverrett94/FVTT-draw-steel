4de1a42b26214fdc4b295828fd0cd505b99930c2
Kody Verrett <cdverrett94@gmail.com>
2024-03-12 18:58:49 -0500

    improvements to class sheet

--- module/models/class.js
+++ module/models/class.js
@@ -6,8 +6,12 @@ export class ClassData extends BaseItemData {
         return {
             resources: new fields.ArrayField(
                 new fields.SchemaField({
-                    name: new fields.StringField(),
-                    max: new fields.StringField(),
+                    name: new fields.StringField({
+                        label: 'system.items.class.fields.resources.name.label',
+                    }),
+                    max: new fields.StringField({
+                        label: 'system.items.class.fields.resources.max.label',
+                    }),
                 })
             ),
             description: new fields.StringField(),
