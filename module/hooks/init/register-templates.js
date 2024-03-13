c4c00da6016aee1ab9eebe4e1f30c5885abd0e1e
Kody Verrett <cdverrett94@gmail.com>
2024-03-12 22:12:26 -0500

    update templates for file moves

--- module/hooks/init/register-templates.js
+++ module/hooks/init/register-templates.js
@@ -1,22 +1,26 @@
 export function registerTemplates() {
     // Load templates
     const templatePaths = [
-        // Base
-        'systems/mcdmrpg/templates/documents/actors/base/partials/abilities.hbs',
-        'systems/mcdmrpg/templates/documents/actors/base/partials/abilities-filter.hbs',
+        // Partials
+        'systems/mcdmrpg/templates/documents/partials/abilities.hbs',
+        'systems/mcdmrpg/templates/documents/partials/abilities-filter.hbs',
         'systems/mcdmrpg/templates/documents/partials/item-header.hbs',
 
         // Heroes
-        'systems/mcdmrpg/templates/documents/actors/hero/partials/skills.hbs',
-        'systems/mcdmrpg/templates/documents/actors/hero/partials/skill.hbs',
-        'systems/mcdmrpg/templates/documents/actors/hero/partials/sidebar.hbs',
-        'systems/mcdmrpg/templates/documents/actors/hero/partials/header.hbs',
-        'systems/mcdmrpg/templates/documents/actors/hero/partials/effects.hbs',
+        'systems/mcdmrpg/templates/documents/hero/skills.hbs',
+        'systems/mcdmrpg/templates/documents/hero/skill.hbs',
+        'systems/mcdmrpg/templates/documents/hero/sidebar.hbs',
+        'systems/mcdmrpg/templates/documents/hero/header.hbs',
+        'systems/mcdmrpg/templates/documents/hero/effects.hbs',
 
         // Ancestry
         'systems/mcdmrpg/templates/documents/ancestry/ancestry-size.hbs',
         'systems/mcdmrpg/templates/documents/ancestry/ancestry-details.hbs',
         'systems/mcdmrpg/templates/documents/ancestry/ancestry-description.hbs',
+
+        //Class
+        'systems/mcdmrpg/templates/documents/class/class-resources.hbs',
+        'systems/mcdmrpg/templates/documents/class/class-details.hbs',
     ];
     loadTemplates(templatePaths);
 }
