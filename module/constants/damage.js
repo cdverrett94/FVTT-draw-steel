48b75222b1b6dbd9e5065a9832b318b41ab8224f
Kody Verrett <cdverrett94@gmail.com>
2024-03-21 15:13:45 -0500

    add icons to damage types and use those icons on the effects tab under ongoing damage

--- module/constants/damage.js
+++ module/constants/damage.js
@@ -2,43 +2,53 @@ export const DAMAGE = {
     TYPES: {
         untyped: {
             label: 'system.damage.types.untyped.label',
-            color: '#000000',
+            color: '#ffffff',
+            icon: '<i class="fa-solid fa-x"></i>',
         },
         acid: {
             label: 'system.damage.types.acid.label',
             color: '#d2ff46',
+            icon: '<i class="fa-solid fa-droplet"></i>',
         },
         cold: {
             label: 'system.damage.types.cold.label',
             color: '#33d6c3',
+            icon: '<i class="fa-solid fa-snowflake"></i>',
         },
         corruption: {
             label: 'system.damage.types.corruption.label',
-            color: '#2f0a28',
+            color: '#a9729f',
+            icon: '<i class="fa-solid fa-skull"></i>',
         },
         fire: {
             label: 'system.damage.types.fire.label',
             color: '#ff5a00',
+            icon: '<i class="fa-solid fa-fire"></i>',
         },
         holy: {
             label: 'system.damage.types.holy.label',
             color: '#fdc723',
+            icon: '<i class="fa-solid fa-face-smile-halo"></i>',
         },
         lightning: {
             label: 'system.damage.types.lightning.label',
-            color: '#1e3e76',
+            color: '#2b78ff',
+            icon: '<i class="fa-solid fa-bolt-lightning"></i>',
         },
         poison: {
             label: 'system.damage.types.poison.label',
             color: '#95ba3e',
+            icon: '<i class="fa-solid fa-flask-round-poison"></i>',
         },
         psychic: {
             label: 'system.damage.types.psychic.label',
             color: '#b080d0',
+            icon: '<i class="fa-regular fa-head-side-brain"></i>',
         },
         sonic: {
             label: 'system.damage.types.sonic.label',
             color: '#cbcbcb',
+            icon: '<i class="fa-solid fa-waveform-lines"></i>',
         },
     },
 };
