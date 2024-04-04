e27d5988a1217269a211167dd6dacc9f2b1d41be
Kody Verrett <cdverrett94@gmail.com>
2024-04-03 23:33:18 -0500

    misc

--- module/rolls/_index.js
+++ module/rolls/_index.js
@@ -1,2 +1,6 @@
+export { AbilityPowerRollDialog } from './ability/roll-dialog.js';
+export { PowerRollDialog } from './power/roll-dialog.js';
+export { TestPowerRollDialog } from './test/roll-dialog.js';
+
 export { DamageRoll } from './damage/damage-roll.js';
 export { PowerRoll } from './power/power-roll.js';
