4dec6e8f9317648b85064a4931ebea299db0013a
Kody Verrett <cdverrett94@gmail.com>
2024-05-08 21:38:13 -0500

    have monster token size update when actor size changes

--- module/documents/monster.js
+++ module/documents/monster.js
@@ -1,3 +1,28 @@
 import { BaseActor } from './base-actor.js';
 
-export class MonsterActor extends BaseActor {}
+export class MonsterActor extends BaseActor {
+    async _preUpdate(changed, options, user) {
+        // update prototype token size when size is updated
+        let prototypeChanged = false;
+        if (changed.system && 'width' in changed.system.size) {
+            changed.prototypeToken ??= {};
+            changed.prototypeToken.width = changed.system.size.width;
+            prototypeChanged = true;
+        }
+        if (changed.system && 'length' in changed.system.size) {
+            changed.prototypeToken ??= {};
+            changed.prototypeToken.height = changed.system.size.length;
+            prototypeChanged = true;
+        }
+
+        await super._preUpdate(changed, options, user);
+
+        if (prototypeChanged) {
+            for (const token of this.getActiveTokens(true, true)) {
+                const keep = ['height', 'width'];
+                const prototypeChanges = Object.fromEntries(Object.entries(changed.prototypeToken).filter(([k]) => keep.includes(k)));
+                await token.update(prototypeChanges);
+            }
+        }
+    }
+}
