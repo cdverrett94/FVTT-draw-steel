f4135a469f7fd58c7c72a8d3328a3c75663d44a6
Kody Verrett <cdverrett94@gmail.com>
2024-05-30 12:06:44 -0500

    refactor PowerRoll to take a pre constructed formula which allows for the use of subclasses

--- module/rolls/power/power-roll.js
+++ module/rolls/power/power-roll.js
@@ -1,19 +1,11 @@
-import { CHARACTERISTICS } from '../../constants/characteristics.js';
 import { TIERS } from '../../constants/power-tier.js';
 
 export class PowerRoll extends Roll {
-    constructor(characteristic, data = {}, options = {}) {
+    constructor(formula, data = {}, options = {}) {
         const modifiers = PowerRoll.combineModifiers(options.modifiers);
         let modifier;
         const difference = modifiers.edges - modifiers.banes;
         modifier = Math.abs(difference) === 1 ? Math.sign(modifiers.edges - modifiers.banes) * 2 : 0;
-        let formula = '2d10';
-
-        if (characteristic in CHARACTERISTICS) {
-            formula = `${formula} + @${characteristic}[${game.i18n.localize('system.characteristics.' + characteristic + '.label').toLowerCase()}]`;
-        }
-        if (modifier) formula = `${formula} + ${modifier}[${modifier < 0 ? 'bane' : 'edge'}]`;
-        if (modifiers.bonuses) formula = `${formula} + ${modifiers.bonuses}[${modifiers.bonuses < 0 ? 'penalties' : 'bonuses'}]`;
 
         super(formula, data, options);
 
@@ -35,15 +27,13 @@ export class PowerRoll extends Roll {
                 break;
         }
         this.modifiedTier = ['doubleBane', 'doubleEdge'].includes(this.modifierType);
-        this.characteristic = characteristic;
+        this.characteristic = options.characteristic;
         this.ability = options.ability;
         this.actor = options.actor;
         this.modifiers = modifiers;
         this.modifier = modifier;
         this.options.rollOptions ??= [];
         this.options.rollOptions.push(`roll:modifier-type:${this.modifierType}`);
-
-        this._formula = this.resetFormula();
     }
 
     get rollOptions() {
@@ -97,10 +87,6 @@ export class PowerRoll extends Roll {
         return this.terms.find((term) => term.formula === '2d10')?.total === 20;
     }
 
-    get formula() {
-        return this.constructor.getFormula(this.terms);
-    }
-
     static CHAT_TEMPLATE = 'systems/mcdmrpg/templates/chat-messages/power-roll.hbs';
 
     static combineModifiers(sources) {
@@ -136,8 +122,48 @@ export class PowerRoll extends Roll {
         return tier;
     }
 
-    get abilityTierEffect() {
-        if (!this.ability) return null;
-        return this.ability.getTierEffect(this.tier);
+    static constructFinalFormula({ formulaParts = [], rollData = {} } = {}) {
+        let formula = '';
+        formulaParts = [{ term: '2d10' }, ...this.baseFormulaParts(rollData), ...formulaParts];
+
+        for (const part of formulaParts) {
+            part.sign ??= '+';
+            part.term ??= 0;
+            formula = `${formula} ${part.sign} ${part.term}`;
+            if (part.flavor) formula = `${formula}[${part.flavor}]`;
+        }
+
+        return formula;
+    }
+
+    static baseFormulaParts(rollData) {
+        console.log(rollData);
+        const formulaParts = [];
+        if (rollData.characteristic) {
+            formulaParts.push({
+                term: `@${rollData.characteristic}`,
+                flavor: game.i18n.localize(`system.characteristics.${rollData.characteristic}.label`).toLowerCase(),
+            });
+        }
+
+        const modifiers = PowerRoll.combineModifiers(rollData.modifiers);
+        const difference = modifiers.edges - modifiers.banes;
+        const modifier = Math.abs(difference) === 1 ? Math.sign(modifiers.edges - modifiers.banes) * 2 : 0;
+
+        if (modifier) {
+            formulaParts.push({
+                term: modifier,
+                flavor: game.i18n.localize(`system.dice.${modifier < 0 ? 'bane' : 'edge'}.singular`).toLowerCase(),
+            });
+        }
+
+        if (modifiers.bonuses) {
+            formulaParts.push({
+                term: modifiers.bonuses,
+                flavor: game.i18n.localize(`system.dice.${modifiers.bonuses < 0 ? 'penalty' : 'bonus'}.plural`).toLowerCase(),
+            });
+        }
+
+        return formulaParts;
     }
 }
