1419e00920505304a2cf53fa990c3285ccc57457
Kody Verrett <cdverrett94@gmail.com>
2024-05-30 13:50:27 -0500

    remove unneeded parts from AbilityRoll

--- module/rolls/ability/ability-roll.js
+++ module/rolls/ability/ability-roll.js
@@ -1,12 +1,6 @@
 import { PowerRoll } from '../_index.js';
 
 export class AbilityRoll extends PowerRoll {
-    constructor(formula, data = {}, options = {}) {
-        super(formula, data, options);
-    }
-
-    static type = 'ability';
-
     static CHAT_TEMPLATE = 'systems/mcdmrpg/templates/chat-messages/ability-message.hbs';
 
     get abilityTierEffect() {
