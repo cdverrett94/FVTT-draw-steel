c82f4045461b7fc820274ce574f72dfab26179e6
Kody Verrett <cdverrett94@gmail.com>
2024-05-29 23:08:56 -0500

    Update ability.js

--- module/documents/ability.js
+++ module/documents/ability.js
@@ -1,4 +1,4 @@
-import { AbilityPowerRollDialog } from '../rolls/_index.js';
+import { AbilityRollDialog } from '../rolls/_index.js';
 import { Predicate } from '../rules/predicate.js';
 import { BaseItem } from './base-item.js';
 
@@ -18,16 +18,8 @@ export class AbilityItem extends BaseItem {
         if (!this.isRollable || !this.parent) return;
 
         const rollOptions = [];
-        rollOptions.push(...this.parent.rollOptions());
-        game.user.targets.forEach((target) => {
-            rollOptions.push(...target.actor.rollOptions('target'));
-            const isFlanking = this.parent.getActiveTokens(true, true).some((token) => {
-                return token.isFlanking({ target: target.document });
-            });
-            if (isFlanking) rollOptions.push('target:flanked');
-        });
-
         rollOptions.push(...this.rollOptions());
+        rollOptions.push(...this.parent.rollOptions());
 
         const rollData = {
             actor: this.parent,
@@ -36,32 +28,34 @@ export class AbilityItem extends BaseItem {
             characteristic: this.system.power.characteristic,
             targets: game.user.targets.reduce((targets, target) => {
                 const targetActor = target.actor;
-                const targetRollOptions = [...rollOptions];
-                targetRollOptions.push(...target.actor.rollOptions('target'));
+                const targetRollOptions = [...target.actor.rollOptions('target')];
 
-                const isFlanked = this.parent.getActiveTokens(true, true).some((token) => {
+                const isFlanking = this.parent.getActiveTokens(true, true).some((token) => {
                     return token.isFlanking({ target: target.document });
                 });
-                if (isFlanked) targetRollOptions.push('target:flanked');
+                if (isFlanking) targetRollOptions.push('target:flanked');
 
-                const targetModifiers = this.#getTargetModifiers(targetActor);
+                const targetModifiers = this.#getTargetModifiers(targetActor, { targetRollOptions, actorRollOptions: this.parent.rollOptions() });
                 targets[targetActor.uuid] = {
                     ...targetModifiers,
                     actor: targetActor,
                     token: target.document,
-                    rollOptions: [...targetRollOptions],
+                    rollOptions: targetRollOptions,
                 };
                 return targets;
             }, {}),
             general: {
                 ...this.#getOwnerModifiers(),
+                rollOptions,
             },
         };
 
-        new AbilityPowerRollDialog(rollData).render(true);
+        new AbilityRollDialog(rollData).render(true);
     }
 
     get isRollable() {
+        if (this.system.power.isResistance) return false;
+
         if (this.system.power.tiers.one.length || this.system.power.tiers.two.length || this.system.power.tiers.three.length) return true;
         else return false;
     }
@@ -76,7 +70,7 @@ export class AbilityItem extends BaseItem {
         return rollData;
     }
 
-    #getTargetModifiers(target) {
+    #getTargetModifiers(target, { targetRollOptions, actorRollOptions } = {}) {
         let rollData = {
             edges: 0,
             banes: 0,
@@ -85,8 +79,8 @@ export class AbilityItem extends BaseItem {
         // Get Edges/Banes that apply when target is attacked
         if (target.system.edges.attacked) rollData.edges += target.system.edges.attacked;
         if (target.system.banes.attacked) rollData.banes += target.system.banes.attacked;
-        const actorRollOptions = this.parent.rollOptions();
-        const targetRollOptions = target.rollOptions('target');
+        actorRollOptions ??= this.parent.rollOptions();
+        targetRollOptions ??= target.rollOptions('target');
         // Get Edges if attacking a creature you are frightened by
         const attackingFrightenedByPredicate = new Predicate([`actor:condition:frightened:${target.uuid}`], actorRollOptions);
         if (attackingFrightenedByPredicate.validate()) rollData.banes += 1;
@@ -96,6 +90,9 @@ export class AbilityItem extends BaseItem {
         // Get Banes if attacking a creature a creature other than one that has you taunted
         const attackingNontauntedPredicate = new Predicate(['actor:condition:taunted', { not: [`actor:condition:taunted`, target.uuid] }], actorRollOptions);
         if (attackingNontauntedPredicate.validate()) rollData.banes += 1;
+
+        const flankingPredicate = new Predicate([`target:flanked`], targetRollOptions);
+        if (flankingPredicate.validate()) rollData.edges += 1;
 
         return rollData;
     }
@@ -128,14 +125,17 @@ export class AbilityItem extends BaseItem {
 
     async toChat() {
         await ChatMessage.create({
+            type: 'ability',
             user: game.user.id,
-            flags: {
-                mcdmrpg: {
-                    ability: this,
+            system: {
+                origin: {
+                    item: this.uuid,
+                    actor: this.parent.uuid,
                 },
             },
             content: await renderTemplate('systems/mcdmrpg/templates/chat-messages/ability-message.hbs', {
                 ability: this,
+                actor: this.parent,
             }),
         });
     }
