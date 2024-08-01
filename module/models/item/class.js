a4d278b211542cd1166bb16a922dda58735631ad
Kody Verrett <cdverrett94@gmail.com>
2024-08-01 18:45:00 -0500

    class sheet work

--- module/models/item/class.js
+++ module/models/item/class.js
@@ -1,3 +1,5 @@
+import { CHARACTERISTICS } from '../../constants/characteristics.js';
+import { getDataModelChoices } from '../../helpers.js';
 import { BaseItemData } from './base-item.js';
 
 export class ClassData extends BaseItemData {
@@ -17,9 +19,49 @@ export class ClassData extends BaseItemData {
                     max: new fields.StringField(),
                 })
             ),
-
-            victoryBenefits: new fields.HTMLField(),
+            characteristics: new fields.SchemaField({
+                starting: new fields.ArrayField(
+                    new fields.SchemaField({
+                        characteristic: new fields.StringField({
+                            choices: getDataModelChoices(CHARACTERISTICS),
+                        }),
+                        value: new fields.NumberField({
+                            integer: true,
+                        }),
+                    }),
+                    {
+                        min: 2,
+                        max: 2,
+                        initial: [
+                            {
+                                characteristic: 'might',
+                                value: 2,
+                            },
+                            {
+                                characteristic: 'agility',
+                                value: 2,
+                            },
+                        ],
+                    }
+                ),
+                arrays: new fields.ArrayField(
+                    new fields.SchemaField({
+                        array: new fields.ArrayField(new fields.NumberField(), {
+                            min: 3,
+                            max: 3,
+                            initial: [0, 0, 0],
+                        }),
+                    }),
+                    { initial: [{ array: [0, 0, 0] }, { array: [0, 0, 0] }, { array: [0, 0, 0] }] }
+                ),
+            }),
+            stamina: new fields.SchemaField({
+                starting: new fields.NumberField(),
+                level: new fields.NumberField(),
+                recoveries: new fields.NumberField(),
+            }),
             resourceGain: new fields.HTMLField(),
+            skillChoices: new fields.ObjectField(),
         };
     }
 }
