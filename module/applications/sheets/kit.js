02418938c16c26e614992f4e2891861d46fc4512
Kody Verrett <cdverrett94@gmail.com>
2024-08-01 18:37:06 -0500

    work on kits for playtest 2

--- module/applications/sheets/kit.js
+++ module/applications/sheets/kit.js
@@ -1,10 +1,11 @@
+import { KITS } from '../../constants/kits.js';
 import { BaseItemSheet } from './base-item.js';
 export class KitSheet extends BaseItemSheet {
     static additionalOptions = {
         classes: ['kit'],
         position: {
-            width: 400,
-            height: 585,
+            width: 500,
+            height: 'auto',
         },
     };
 
@@ -13,10 +14,6 @@ export class KitSheet extends BaseItemSheet {
     static PARTS = foundry.utils.mergeObject(
         super.PARTS,
         {
-            header: {
-                id: 'header',
-                template: 'systems/mcdmrpg/templates/documents/partials/item-header.hbs',
-            },
             details: {
                 id: 'details',
                 template: '/systems/mcdmrpg/templates/documents/kit/kit-details.hbs',
@@ -24,4 +21,12 @@ export class KitSheet extends BaseItemSheet {
         },
         { inplace: false }
     );
+
+    async _prepareContext(options) {
+        const context = await super._prepareContext(options);
+
+        context.constants.kits = KITS;
+
+        return context;
+    }
 }
--- module/models/item/kit.js
+++ module/models/item/kit.js
@@ -1,3 +1,5 @@
+import { KITS } from '../../constants/kits.js';
+import { getDataModelChoices } from '../../helpers.js';
 import { BaseItemData } from './base-item.js';
 
 export class KitData extends BaseItemData {
@@ -5,16 +7,93 @@ export class KitData extends BaseItemData {
     static defineSchema() {
         const fields = foundry.data.fields;
         return {
-            type: new fields.StringField(),
-            armor: new fields.StringField(),
-            weapon: new fields.StringField(),
-            area: new fields.StringField(),
-            stamina: new fields.StringField(),
-            damage: new fields.NumberField(),
-            reach: new fields.StringField(),
-            speed: new fields.StringField(),
-            range: new fields.NumberField(),
-            resistanceTN: new fields.NumberField(),
+            ...super.defineSchema(),
+            type: new fields.StringField({
+                choices: getDataModelChoices(KITS.TYPES),
+            }),
+            armor: new fields.ArrayField(
+                new fields.StringField({
+                    choices: getDataModelChoices(KITS.ARMOR),
+                })
+            ),
+            weapon: new fields.ArrayField(
+                new fields.StringField({
+                    choices: getDataModelChoices(KITS.WEAPON),
+                })
+            ),
+            implement: new fields.ArrayField(
+                new fields.StringField({
+                    choices: getDataModelChoices(KITS.IMPLEMENT),
+                })
+            ),
+
+            stamina: new fields.NumberField({
+                integer: true,
+                min: 0,
+                initial: 0,
+            }),
+            speed: new fields.NumberField({
+                integer: true,
+                min: 0,
+                initial: 0,
+            }),
+            stability: new fields.NumberField({
+                integer: true,
+                min: 0,
+                initial: 0,
+            }),
+
+            damage: new fields.SchemaField({
+                melee: new fields.ArrayField(
+                    new fields.NumberField({
+                        integer: true,
+                        min: 0,
+                    }),
+                    {
+                        initial: [0, 0, 0],
+                        min: 3,
+                        max: 3,
+                    }
+                ),
+                range: new fields.ArrayField(
+                    new fields.NumberField({
+                        integer: true,
+                        min: 0,
+                    }),
+                    {
+                        initial: [0, 0, 0],
+                        min: 3,
+                        max: 3,
+                    }
+                ),
+                magic: new fields.ArrayField(
+                    new fields.NumberField({
+                        integer: true,
+                        min: 0,
+                    }),
+                    {
+                        initial: [0, 0, 0],
+                        min: 3,
+                        max: 3,
+                    }
+                ),
+            }),
+
+            area: new fields.NumberField({
+                integer: true,
+                min: 0,
+                initial: 0,
+            }),
+            reach: new fields.NumberField({
+                integer: true,
+                min: 0,
+                initial: 0,
+            }),
+            distance: new fields.NumberField({
+                integer: true,
+                min: 0,
+                initial: 0,
+            }),
         };
     }
 }
